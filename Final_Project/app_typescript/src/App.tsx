93dd588909f1018cd32c0a9211a7076577512684
ChristopherTanwa-ai <clethjorgensen@gmail.com>
2023-04-27 14:12:28 +0200

    hi

--- Final_Project/app_typescript/src/App.tsx
+++ Final_Project/app_typescript/src/App.tsx
@@ -1,14 +1,30 @@
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
-import HomeProducts from "@/scenes/homeProducts";
-import { useEffect, useState } from "react";
+import HomeProducts from "@/scenes/home/HomeProducts";
+import { useEffect, useState, Fragment } from "react";
 import { Poster, SelectedPage } from "./shared/types";
-
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Shop from "./scenes/shop";
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
   const [isTopOfPage,SetIsTopOfPage] = useState<boolean>(true);
   const [apiResponse, setApiResponse] = useState<Record<string, Poster>>({});
   
+  let component
+  
+  switch (window.location.pathname) {
+    case "/home":
+        component = <Home setSelectedPage={setSelectedPage} apiResponse={apiResponse} />
+      break;
+    case "/":
+        component = <Home setSelectedPage={setSelectedPage} apiResponse={apiResponse} />
+      break;
+    case "/#shop":
+        component = <Shop></Shop>
+      break;
+    default:
+      break;
+  }
 
   
 
@@ -42,16 +58,23 @@ function App() {
 
 
   return (
+  
     <>
       <div className='app bg-200'>
-        <Navbar
+        
+           <Navbar
         isTopOfPage={isTopOfPage}
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage} 
         /> 
-        <Home setSelectedPage={setSelectedPage} />
-       <HomeProducts setSelectedPage={setSelectedPage} selectedPage={selectedPage} apiResponse={apiResponse}/> 
+      
+      {component}      
+            
+        
+         
+     
       </div>
+      
     </>
   )
 }
