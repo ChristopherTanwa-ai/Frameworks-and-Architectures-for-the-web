cf41c67509aad117cf61a7d2e8d5413bf79be04d
“Jakob” <jaken@itu.dk>
2023-05-01 13:14:44 +0200

    ehi

--- Final_Project/app_typescript/src/scenes/navbar/index.tsx
+++ Final_Project/app_typescript/src/scenes/navbar/index.tsx
@@ -1,95 +1,155 @@
-import { useState } from "react"
-import { Bars3Icon,XMarkIcon } from "@heroicons/react/24/solid"
+import { useEffect, useRef, useState } from "react"
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid"
 import Logo from "@/assets/Logo.svg"
 import Link from "./Link"
 import { SelectedPage } from "@/shared/types"
 import useMediaQuery from "@/hooks/useMediaQuery"
 import ActionButton from "@/shared/ActionButton"
-
+import Link2 from "./Link2"
+import { motion } from "framer-motion"
+import Login from "@/scenes/Login";
 
 type Props = {
     isTopOfPage: boolean;
     selectedPage: SelectedPage;
     setSelectedPage: (value: SelectedPage) => void
+
 }
 
-const Navbar = ({isTopOfPage, selectedPage,setSelectedPage}: Props) => {
-const flexbetween = "flex items-center justify-between";
-const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
-const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
-const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow"
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
+    const flexbetween = "flex items-center justify-between";
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+    const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+    const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow"
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const loginRef = useRef<HTMLDivElement>(null);
+    const [showLogin, setShowLogin] = useState(false);
+    const [username, setUsername] = useState<string | null>(null);
 
-  return (
-    <nav>
-        <div className={`${navbarBackground} ${flexbetween} fixed top-0 z-30 w-full py-6`}>
-            <div className={`${flexbetween} mx-auto w-5/6`}>
-                <div className={`${flexbetween} w-full gap-16 `}>
-                    {/* Left side */}
-                    <img src={Logo} alt="logo" className="max-w-8 max-h-8" />
-                
-                    {/* Right side */}
-                   {isAboveMediumScreens ? (
-                   <div className={`${flexbetween} w-full`}>
-                        <div className={`${flexbetween} gap-8 text-sm`}>
-                            <Link page="Home"
-                             selectedPage={selectedPage}
-                             setSelectedPage={setSelectedPage} />
-                            <Link page="Shop"  
-                             selectedPage={selectedPage}
-                             setSelectedPage={setSelectedPage} />
-                            <Link page="About" 
-                             selectedPage={selectedPage}
-                             setSelectedPage={setSelectedPage} />
-                            <Link page="Cart" 
-                             selectedPage={selectedPage}
-                             setSelectedPage={setSelectedPage}/>
-                        </div>
-                        <div className={`${flexbetween} gap-8`}>
-                        <p>Login</p>
-                        <ActionButton setSelectedPage={setSelectedPage}>Sign Up</ActionButton>
-                        </div>
-                    </div>)
-                        :(
-                            <button
-                             className="rounded-full bg-secondary-500 p-2"
-                             onClick={() => {setIsMenuToggled(!isMenuToggled)}}
-                             >
-                        <Bars3Icon className="h-6 w-6 text-white" />
-                            
-                            </button>
-                        )
-                    }
+    const handleLogin = () => {
+        setIsLoggedIn(true);
+        setShowLogin(false);
+        setSelectedPage(SelectedPage.Home);
+        const user = sessionStorage.getItem('user');
+        if (user) {
+            const display = JSON.parse(user);
+            setUsername(display.username);
+        }
+    };
+
+    const handleLoginButtonClick = () => {
+        setIsLoggedIn(false);
+        setShowLogin(!showLogin);
+    };
+
+    const handleClickOutsideLogin = (e: MouseEvent) => {
+        if (loginRef.current && !loginRef.current.contains(e.target as Node)) {
+            setIsLoggedIn(true);
+        }
+    };
+
+    useEffect(() => {
+
+        document.addEventListener("mousedown", handleClickOutsideLogin);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutsideLogin);
+        };
+    }, []);
+    return (
+        <nav>
+            <div className={`${navbarBackground} ${flexbetween} fixed top-0 z-30 w-full py-6`}>
+                <div className={`${flexbetween} mx-auto w-5/6`}>
+                    <div className={`${flexbetween} w-full gap-16 `}>
+                        {/* Left side */}
+                        <img src={Logo} alt="logo" className="max-w-8 max-h-8" />
+
+                        {/* Right side */}
+                        {isAboveMediumScreens ? (
+                            <div className={`${flexbetween} w-full`}>
+                                <div className={`${flexbetween} gap-8 text-sm`}>
+                                    <Link2 page="Home"
+                                        selectedPage={selectedPage}
+                                        setSelectedPage={setSelectedPage} />
+                                    <Link2 page="Shop"
+                                        selectedPage={selectedPage}
+                                        setSelectedPage={setSelectedPage} />
+                                    <Link page="About"
+                                        selectedPage={selectedPage}
+                                        setSelectedPage={setSelectedPage} />
+                                    <Link page="Cart"
+                                        selectedPage={selectedPage}
+                                        setSelectedPage={setSelectedPage} />
+
+                                    {isLoggedIn ? (
+                                        <p> Welcome {username}</p>
+                                    ) : (
+                                        <button onClick={handleLoginButtonClick}>Login</button>
+                                    )}
+
+                                    {showLogin && (
+                                        <div className="fixed top-20 right-10">
+                                            <Login
+                                                isLoggedIn={isLoggedIn}
+                                                handleLogin={handleLogin}
+                                            />
+                                        </div>
+                                    )}
+                                </div>
+
+
+                            </div>)
+                            : (
+                                <button
+                                    className="rounded-full bg-secondary-500 p-2"
+                                    onClick={() => { setIsMenuToggled(!isMenuToggled) }}
+                                >
+                                    <Bars3Icon className="h-6 w-6 text-white" />
+
+                                </button>
+                            )
+                        }
                     </div>
                 </div>
+
             </div>
             {/** Mobile Menu Modal */}
             {!isAboveMediumScreens && isMenuToggled && (
-                <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+                <motion.div
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ delay: 0.0, duration: 0.3 }}
+                    variants={{
+                        hidden: { opacity: 0, x: 50 },
+                        visible: { opacity: 1, x: 0 }
+                    }}
+                    className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
                     {/**Close icon */}
                     <div className="flex justify-end p-12">
                         <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
-                        <XMarkIcon className="h-6 w-6 text-black" />
+                            <XMarkIcon className="h-6 w-6 text-black" />
                         </button>
                     </div>
                     {/** Menu items */}
                     <div className="ml-[33%] flex flex-col gap-10 text-2xl">
-                            <Link page="Home"
-                             selectedPage={selectedPage}
-                             setSelectedPage={setSelectedPage} />
-                            <Link page="Shop"  
-                             selectedPage={selectedPage}
-                             setSelectedPage={setSelectedPage} />
-                            <Link page="About" 
-                             selectedPage={selectedPage}
-                             setSelectedPage={setSelectedPage} />
-                            <Link page="Cart" 
-                             selectedPage={selectedPage}
-                             setSelectedPage={setSelectedPage}/>
-                        </div>
-                </div>
+                        <Link2 page="Home"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage} />
+                        <Link2 page="Shop"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage} />
+                        <Link page="About"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage} />
+                        <Link page="Cart"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage} />
+                    </div>
+                </motion.div>
             )}
-    </nav>
-  )
+        </nav>
+    )
 }
 
 export default Navbar
