0b4883669dd88858cc14ff9dc4187929b8e7f5e3
ChristopherTanwa-ai <clethjorgensen@gmail.com>
2023-05-01 13:04:08 +0200

    moved

--- BirdsQuiz-initial/counter.js
+++
@@ -1,5 +0,0 @@
-function counter(){
-    let count = 0;
-    count = count+1;
-    localStorage.setItem("count",count)
-}
--- Lesson-7-initial/quiz-service.js
+++
@@ -1,7 +0,0 @@
-import { getData } from "./fetch.js";
-
-// Implement  HTTP request GET method on "/BirdQuiz.json" resource
-export async function getQuiz() {
-  // call getData here and replace the object in the next return statemnt wuth the correct value 
- return getData("./birdQuiz.json")
-}
