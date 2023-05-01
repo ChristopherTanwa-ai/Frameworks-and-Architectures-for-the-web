55a446541c5c420f8ef8ad9faacb4a98b761c2e3
“Jakob” <jaken@itu.dk>
2023-05-01 13:48:50 +0200

    Midlertidig

--- Final_Project/api/app.js
+++ Final_Project/api/app.js
@@ -5,10 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var randomPostersRouter = require("./routes/randomPosters.js");
 var allPoster = require("./routes/allProducts.js");
-var users = require("./routes/users.js")
+const fs = require('fs');
 var app = express();
 
 // view engine setup
@@ -22,10 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users23', usersRouter);
 app.use("/randomPosters", randomPostersRouter);
 app.use("/allPosters", allPoster);
-app.use("/users", users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,6 +39,55 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+//Jakobs hjørne af problemer
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
+
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const users = getUsersFromFile();
+
+  const user = users.find((u) => u.username === username);
+
+  if (user && user.password === password) {
+    res.json({ success: true });
+  } else {
+    res.status(401).json({ success: false, message: 'Incorrect password' });
+  }
+});
+
+app.post('/users', (req, res) => {
+  const { username, password } = req.body;
+  const users = getUsersFromFile();
+
+  const existingUser = users.find((u) => u.username === username);
+
+  if (existingUser) {
+    res.status(409).json({ success: false, message: 'Username already exists' });
+  } else {
+    users.push({ username, password, basket: [] });
+    saveUsersToFile(users);
+    res.json({ success: true });
+  }
+});
+
+app.get('/users', (req, res) => {
+  const users = getUsersFromFile();
+  res.json(users);
+});
+
+function getUsersFromFile() {
+  const users = JSON.parse(fs.readFileSync('users.json', 'utf8'));
+  return users;
+}
+
+function saveUsersToFile(users) {
+  fs.writeFileSync('users.json', JSON.stringify(users), 'utf8');
+}
 
 module.exports = app;
 
